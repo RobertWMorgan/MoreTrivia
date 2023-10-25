0aa3c2304f54dc1617a0357780113a0a9e183286
Dhaval Pandhi <dhavalpandhi@gmail.com>
2023-10-26 00:58:25 +0800

    added html and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
 import './App.scss';
+import { useState } from "react";
 
+
+// btnList.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         document.querySelector('.active')?.classList.remove('active');
+//         btn.classList.add('active');
+//     })
+// })
+
+// const darkToggle = document.getElementById("dark-mode-toggle")
+
+// darkToggle.onclick = function() {
+//     document.body.classList.toggle("darkmode")
+// }
 
 
 function App() {
+
+  const [isDark, setIsDark] = useState("lightmode");
+
+  const darkToggle = () => {
+    if (isDark === "lightmode") {
+      setIsDark("darkmode")
+    } else setIsDark("lightmode")
+    console.log(isDark)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class={isDark === "lightmode" ? "background-lightmode" : "background-darkmode"}>
+      <div class="header">
+        <h1 class={isDark === "lightmode" ? "lightmode" : "darkmode"}>More Trivia</h1>
+        <div class="header-right">
+                <button id="dark-mode-toggle" onClick={darkToggle}><image src="/images/darkmode-icon.png" /></button>
+        </div>
+      </div>
+      <div class="container">
+        <div class="difficulty-buttons">
+            <button class={isDark === "lightmode" ? "lightmode" : "darkmode"} id="easy">Easy</button>
+            <button id="medium" class={isDark === "lightmode" ? "lightmode" : "darkmode"}>Medium</button>
+            <button id="hard" class={isDark === "lightmode" ? "lightmode" : "darkmode"}>Hard</button>
+        </div>
+        <div class="user-input">
+            <input type="text" name="user-input" id="user-input" placeholder="What topic would you like?" class={isDark === "lightmode" ? "lightmode" : "darkmode"}/>
+        </div>
+      </div>
+    </main>
   );
 }
 
